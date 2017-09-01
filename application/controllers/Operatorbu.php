fce0d15a9cbb757c7790944af584327e78b7bab8
Rudini Nor Habibi <sorows22@gmail.com>
2017-09-01 23:41:32 +0800

    ralat

--- application/controllers/Operatorbu.php
+++ application/controllers/Operatorbu.php
@@ -9,44 +9,7 @@ class Operatorbu extends Operatorbu_C
         $this->load->view('operatorbu/index');
         $this->load->view('operatorbu/inc/south');
 
-        $this->load->library('email');
-
-$subject = 'This is a test';
-$message = '<p>This message has been sent for testing purposes.</p>';
-
-// Get full html:
-$body = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=' . strtolower(config_item('charset')) . '" />
-    <title>' . html_escape($subject) . '</title>
-    <style type="text/css">
-        body {
-            font-family: Arial, Verdana, Helvetica, sans-serif;
-            font-size: 16px;
-        }
-    </style>
-</head>
-<body>
-' . $message . '
-</body>
-</html>';
-// Also, for getting full html you may use the following internal method:
-//$body = $this->email->full_html($subject, $message);
-
-$result = $this->email
-        ->from('yourusername@gmail.com')
-        ->reply_to('yoursecondemail@somedomain.com')    // Optional, an account where a human being reads.
-        ->to('therecipient@otherdomain.com')
-        ->subject($subject)
-        ->message($body)
-        ->send();
-
-var_dump($result);
-echo '<br />';
-echo $this->email->print_debugger();
-
-exit;
+      
     }
 
     public function input()
