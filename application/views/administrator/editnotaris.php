3c219507d17988a7238ad0433fbcf4ff64a43d75
Khairul Fajeri <30503882+khairulfajeri14@users.noreply.github.com>
2017-09-08 00:11:50 +0800

    Add files via upload

---
+++ application/views/administrator/editnotaris.php
@@ -0,0 +1,52 @@
+<div class="easyui-tabs">
+    <div title="Input Data Reakanan" style="padding-left:10px; padding-right:20px; ">
+
+ <form action="<?= base_url() ?>Administrator/editnotaris/<?= $value->id; ?>" method="post">
+        <div style="margin:20px 0;"></div>
+         <?php $this->load->view('_partial/flash_message') ?>
+        <div class="row">
+            <!-- left column -->
+            <div class="col-md-12">
+              <!-- general form elements -->
+             
+                <!-- form start -->
+                
+                <div class="box-header with-border">
+                  <h3 class="box-title">Edit Rekanan Notaris</h3>
+                </div>
+
+                <div class="col-md-4"></div>
+                  <div class="col-md-4">
+                  <div class="box box-info">
+                  <div class="box-body">
+                    <div class="form-group">
+                      <label>Nama Rekanan</label>
+                      <input value="<?= $value->nama; ?>" type="text" required="1" name="nama" class="form-control" placeholder="Enter Nama Rekanan">
+                    </div>
+                   
+                    <div class="form-group">
+                      <label>Email</label>
+                      <input value="<?= $value->email; ?>" required="1" type="email" name="email" class="form-control" placeholder="Enter Email Rekanan">
+                    </div>
+                    <div class="form-group">
+                      <label>Limit Kredit</label>
+                      <input value="<?= $value->limit_kredit; ?>" type="text" name="limit_kredit" required="1" class="form-control" placeholder="Enter Limit Kredit">
+                    </div>
+                    <div class="box-footer">
+                    <button type="submit" name="submit" class="btn btn-success control-group pull-right">Submit</button>
+                  </div><!-- /.box-footer -->
+                   
+                    </div>
+                    <div class="col-md-4"></div>
+                    
+                  </div><!-- /.box-body -->
+
+                 </div>
+           
+                </div><!-- /.col-md-12 -->
+             </div><!-- /.row -->
+        </form>
+
+    </div>
+</div>
+    
---
+++ application/views/administrator/listnotaris.php
@@ -0,0 +1,55 @@
+<div class="easyui-tabs">
+    <div title="Data Rekanan" style="padding-left:10px; padding-right:20px; ">
+	<div class="row">
+            <div class="col-xs-12">
+            <?php $this->load->view('_partial/flash_message') ?>
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Data Rekanan Notaris</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <table id="example2" class="table table-bordered table-hover">
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Limit Kredit</th>
+                        <th>Jenis Order</th>
+                        <th width="18%">Action</th>
+                       
+                      </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $i=0;
+                      foreach ($notaris as $value) {
+                        
+                        $i++;
+                    ?>
+
+
+                      <tr>
+                        <td><?= $i; ?></td>
+                        <td><?= $value->nama; ?></td>
+                        <td><?= $value->email; ?></td>
+                        <td><?= $value->limit_kredit; ?></td>
+                        <td><?= $value->jenis_order; ?></td>
+                        <td><a href="#"<button class="btn btn-info">Detail</button> </a> <a href="<?= base_url(); ?>Administrator/editnotaris/<?= $value->id; ?>"<button class="btn btn-success">Edit</button> </a> <a onclick="return confirm('Yakin hapus <?= $value->nama; ?> ?')" href="<?= base_url(); ?>Administrator/deletenotaris/<?= $value->id; ?>"<button class="btn btn-danger">Hapus</button> </a></td>
+                        
+                      </tr>
+
+                    <?php
+                      }
+                    ?>
+                    </tbody>
+                    </tfoot>
+
+                  </table>
+                </div><!-- /.box-body -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+
+    </div>
+</div>
