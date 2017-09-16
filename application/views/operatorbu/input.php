9916f5d0b1de9bec2591078dd0ddc2eae43c0656
Rudini Nor Habibi <sorows22@gmail.com>
2017-09-17 04:39:08 +0800

    bu
    bu

--- application/views/operatorbu/input.php
+++ application/views/operatorbu/input.php
@@ -42,19 +42,20 @@
                 
 
   <div style="margin:1px 0;"></div>
-  <div class="easyui-panel" title="New Topic" style="width:100%;max-width:400px;padding:30px 60px;">
+  <div class="easyui-panel" title="Data Order" style="width:100%;max-width:500px;padding:1px 1px;">
     <form id="ff" class="easyui-form" method="post" data-options="novalidate:true">
-      <div style="margin-bottom:20px">
-        <input class="easyui-textbox" name="name" style="width:100%" data-options="label:'Name:',required:true">
+      <div style="margin-bottom:10px">
+        <input class="easyui-textbox" name="name" style="width:100%" data-options="label:'Nama Debitur:',required:true">
       </div>
       <div style="margin-bottom:20px">
-        <input class="easyui-textbox" name="email" style="width:100%" data-options="label:'Email:',required:true,validType:'email'">
+        <input class="easyui-textbox" name="email" style="width:100%" data-options="label:'Limit Kredit:',required:true,validType:'email'">
       </div>
       <div style="margin-bottom:20px">
-        <input class="easyui-textbox" name="subject" style="width:100%" data-options="label:'Subject:',required:true">
+        <input class="easyui-textbox" name="subject" style="width:100%" data-options="label:'Segmen Kredit:',required:true">
       </div>
       <div style="margin-bottom:20px">
-        <input class="easyui-textbox" name="message" style="width:100%;height:60px" data-options="label:'Message:',multiline:true">
+
+        <input class="easyui-textbox" name="message" style="width:100%;height:60px" data-options="label:'Jenis Order:',labelWidth:100,multiline:true">
       </div>
       <div style="margin-bottom:20px">
         <select class="easyui-combobox" name="language" label="Language" style="width:100%"><option value="ar">Arabic</option><option value="bg">Bulgarian</option><option value="ca">Catalan</option><option value="zh-cht">Chinese Traditional</option><option value="cs">Czech</option><option value="da">Danish</option><option value="nl">Dutch</option><option value="en" selected="selected">English</option><option value="et">Estonian</option><option value="fi">Finnish</option><option value="fr">French</option><option value="de">German</option><option value="el">Greek</option><option value="ht">Haitian Creole</option><option value="he">Hebrew</option><option value="hi">Hindi</option><option value="mww">Hmong Daw</option><option value="hu">Hungarian</option><option value="id">Indonesian</option><option value="it">Italian</option><option value="ja">Japanese</option><option value="ko">Korean</option><option value="lv">Latvian</option><option value="lt">Lithuanian</option><option value="no">Norwegian</option><option value="fa">Persian</option><option value="pl">Polish</option><option value="pt">Portuguese</option><option value="ro">Romanian</option><option value="ru">Russian</option><option value="sk">Slovak</option><option value="sl">Slovenian</option><option value="es">Spanish</option><option value="sv">Swedish</option><option value="th">Thai</option><option value="tr">Turkish</option><option value="uk">Ukrainian</option><option value="vi">Vietnamese</option></select>
